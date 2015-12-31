7bb3f1f8e579f10f11795d774ebc5443a5f6d548
root <hawk.alan@gmail.com>
2015-12-31 23:46:23 +0000

    change

--- Main.hs
+++ Main.hs
@@ -10,9 +10,13 @@ import System.IO
 main = do
   -- simpleHttp "https://en.wikipedia.org/wiki/Main_Page"
   handle <- openFile "Main_Page.html" ReadMode
+  handleOut <- openFile "out.html" WriteMode
   hSetEncoding handle utf8_bom
+  hSetEncoding handleOut utf8_bom
   contents <- hGetContents handle
-  let html = renderTags $ parseTags contents
+  let html = renderTags $ flattenTree $ transformTree f $  tagTree $ parseTags contents
   hSetEncoding stdout utf8_bom
-  putStr html
-  
+  hPutStr handleOut html
+ 
+f (TagBranch "div" atts inner ) = [TagBranch "p" atts inner]
+f x = [x]
