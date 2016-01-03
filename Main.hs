2ebc32dd31a0ee2cd2986267e5d239012dae757f
root <hawk.alan@gmail.com>
2016-01-03 02:54:34 +0000

    added close

--- Main.hs
+++ Main.hs
@@ -7,45 +7,36 @@ import Text.HTML.TagSoup.Tree
 import System.IO
 import Control.Monad.RWS
 import Data.Generics.Uniplate.Direct
+import Debug.Trace
 
 main = do
-  -- simpleHttp "https://en.wikipedia.org/wiki/Main_Page"
   handle <- openFile "testinput.html" ReadMode
   handleOut <- openFile "out.html" WriteMode
   hSetEncoding handle utf8_bom
   hSetEncoding handleOut utf8_bom
   contents <- hGetContents handle
-  let html = renderTags $  flattenTree $ (:[])$ transform f $ head $  tagTree $ parseTags contents
+  let html = renderTags $ flattenTree $ (:[]) $ transform f $ head $ tagTree $ parseTags contents
   hSetEncoding stdout utf8_bom
   hPutStr handleOut html
+  hClose handleOut
 
-f subtree 
- | isContent subtree = (TagLeaf (TagText "blah"))
- | otherwise = subtree 
-
-isContent subtree =
- not (containsContainers flatTree)
- && not(isAllWhiteSpace flatTree)
+f t@(TagBranch name atts ts)  
+ | (not (any containsContainers fts)) && (not (all allWhiteSpace fts))
+    = (TagBranch name atts [(TagLeaf (TagText "blah"))])
+ | otherwise = t
  where 
-  flatTree = flattenTreeEasy subtree
+  fts = map flattenTreeEasy ts
+f x = x
 
 containsContainers = 
-   (not . null) .
-   filter (or . mapM isTagOpenName ["div","table","td","tr","thead","tbody"])
+ any (or . mapM isTagOpenName ["div","table","td","tr","thead","tbody"])
 
-
-isAllWhiteSpace = 
-  and . 
-  map (or . sequence [isTagOpenName "br" , allSpace . innerText . (:[]) ])
-
-allSpace = all isSpace
+allWhiteSpace flatTree = 
+    (all (or . sequence [isTagOpenName "br",isTagText]) flatTree)
+ && (all isSpace . innerText $ flatTree)
 
 flattenTreeEasy x = flattenTree [x] 
  
 instance Uniplate (TagTree a) where
-  uniplate (TagBranch x1 x2 x3) = plate (TagBranch x1 x2) ||* x3                                                                                                
+  uniplate (TagBranch x1 x2 x3) = plate (TagBranch x1 x2) ||* x3
   uniplate x = plate x   
-
-
-
-
